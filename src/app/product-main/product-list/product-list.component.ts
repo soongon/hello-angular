7c328e032d9be8c78cf99ad6d6326af48a9dab9f
soongonkim <soongon@gmal.com>
2022-04-14 10:50:57 +0900

    initial commit

---
+++ src/app/product-main/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  
+  products = ['아이폰13', '갤럭시22', '모니터27'];
+
+}
---
+++ src/app/product-main/product-title/product-title.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-title',
+  templateUrl: './product-title.component.html',
+  styleUrls: ['./product-title.component.css']
+})
+export class ProductTitleComponent {
+
+  title: string = '상품목록잉;요';
+
+  buttonClicked() {
+    console.log('button clicked..');
+    this.title = '버튼 클릭했어요..';
+  }
+
+}
